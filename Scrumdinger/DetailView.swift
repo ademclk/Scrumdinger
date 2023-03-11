95f99f6d6a4b85861fad5f4f5b175e713334a3ef
ademclk <ademonurcelik@icloud.com>
2023-03-11 21:19:07 +0300

    Create: Detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Adem Onur Çelik on 11.03.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
