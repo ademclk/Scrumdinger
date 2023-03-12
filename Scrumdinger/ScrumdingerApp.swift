32033550b0327f16d3a5b730c3e2cbc48ef4f94f
ademclk <ademonurcelik@icloud.com>
2023-03-13 00:09:14 +0300

    Add: Save data in inactive state

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,10 +10,17 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: $store.scrums)
+                ScrumsView(scrums: $store.scrums) {
+                    ScrumStore.save(scrums: store.scrums) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
             }
             .onAppear {
                 ScrumStore.load { result in
